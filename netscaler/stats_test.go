2d1b6e4243bd390fbec036c42fb159ff0a1bd154
Xin Huang <xhuang@freewheel.tv>
2018-06-19 13:56:52 -0400

    Add a testcase for findstats function

--- netscaler/stats_test.go
+++ netscaler/stats_test.go
@@ -2,9 +2,23 @@ package netscaler
 
 import "testing"
 
-func TestNitroClient_FindStat(t *testing.T) {
+func TestNitroClient_FindAllStats(t *testing.T) {
 	_, err := client.FindAllStats("lbvserver")
 	if err != nil {
 		t.Error("Did not find statistics of type ", err, Lbvserver.Type())
+	}
+}
+
+func TestNitroClient_FindStats(t *testing.T) {
+	for _, resourceType := range []string{"lbvserver", "service", "gslbvserver", "gslbservice"} {
+		rsrc, err := client.FindAllResources("i")
+		if err != nil {
+			// Ignore the erratic resource type
+			continue
+		}
+		_, err = client.FindStat(resourceType, rsrc[0]["name"].(string))
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 }
