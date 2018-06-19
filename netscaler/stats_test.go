edeb2388655bec85abb5bc3701b87c70967f2f84
Xin Huang <xhuang@freewheel.tv>
2018-06-19 14:06:01 -0400

    Change resource type reference

--- netscaler/stats_test.go
+++ netscaler/stats_test.go
@@ -3,7 +3,7 @@ package netscaler
 import "testing"
 
 func TestNitroClient_FindAllStats(t *testing.T) {
-	_, err := client.FindAllStats("lbvserver")
+	_, err := client.FindAllStats(Lbvserver.Type())
 	if err != nil {
 		t.Error("Did not find statistics of type ", err, Lbvserver.Type())
 	}
